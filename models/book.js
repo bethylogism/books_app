94167182b4650c1c627ef53b27eeae85dfc831ee
laceswingybethler <beth.swingler@gmail.com>
2019-02-04 18:50:05 +0000

    Fixing link

--- models/book.js
+++ models/book.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true},
-  releaseDate: { type: Date },
-  genre: { type: String },
-  image: { type: String }
+  image: { type: String, required: true },
+  releaseDate: { type: String },
+  genre: { type: String }
 })
 
 module.exports = mongoose.model('Book', bookSchema)
